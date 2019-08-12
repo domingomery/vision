2768f0960412f5f004fcc065876072560f1e6562
Domingo Mery <domingo.mery@uc.cl>
2019-08-12 09:57:41 -0400

    ...

--- clases/Cap02_Geometria/matlab/CV02_Trans2D.m
+++ clases/Cap02_Geometria/matlab/CV02_Trans2D.m
@@ -1,4 +1,5 @@
 disp('1. go to folder with file painting.png (example: cd ../images)')
 disp('2. run Bmv_guihomography (function of Balu Toolbox)')
-disp('3. press bottom Input')
-disp([4. 'type ' char(39) 'painting.png' char (39) ' (with apostrophes)'])
+disp('3. press bottom Load')
+disp(['4. type ' char(39) 'painting.png' char (39) ' (with apostrophes)'])
+disp('5. play with the buttons of the GUI to rotate, translate and change the perspective oof the painting') 
