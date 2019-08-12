66cf5262e29f18457b3837fcfc9592bf893ae171
Domingo Mery <domingo.mery@uc.cl>
2019-08-12 09:54:11 -0400

    Create CV02_Trans2D.m

---
+++ clases/Cap02_Geometria/matlab/CV02_Trans2D.m
@@ -0,0 +1,4 @@
+disp('1. go to folder with file painting.png (example: cd ../images)')
+disp('2. run Bmv_guihomography (function of Balu Toolbox)')
+disp('3. press bottom Input')
+disp([4. 'type ' char(39) 'painting.png' char (39) ' (with apostrophes)'])
